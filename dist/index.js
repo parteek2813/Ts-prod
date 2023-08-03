250e27c89bbee9840aa0c8be6381fbaf448ffef8
parteek kumar <digitalparteek@gmail.com>
2023-08-03 18:46:50 +0530

    Getters and setters

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,45 @@
 "use strict";
+// class User {
+//   email: string;
+//   name: string;
+//   private readonly city: string = "Gohana";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
     constructor(email, name) {
-        this.city = "Gohana";
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
+        this.city = "Gohana";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count must be greater than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const Parteek = new User("p@p.com", "Parteek");
-// Parteek.city = "Gohana";
+// for making the property as private .... we can use the private keyword or the #[hash] between them.....
+// the # comes from js world and private comes from ts world
+/**
+ * Getters and setters
+ * Get is used to get the property .... which can be private property or a
+ * public property
+ *
+ * setter is used to set the property but we can't return anything from the
+ * setter in the ts world and does not write return type in ts in setters
+ *
+ */
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,49 @@
+// class User {
+//   email: string;
+//   name: string;
+//   private readonly city: string = "Gohana";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
-  readonly city: string = "Gohana";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
+  private _courseCount = 1;
+
+  private readonly city: string = "Gohana";
+  constructor(public email: string, public name: string) {}
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count must be greater than 1");
+    }
+    this._courseCount = courseNum;
   }
 }
 
 const Parteek = new User("p@p.com", "Parteek");
-// Parteek.city = "Gohana";
+
+// for making the property as private .... we can use the private keyword or the #[hash] between them.....
+// the # comes from js world and private comes from ts world
+
+/**
+ * Getters and setters
+ * Get is used to get the property .... which can be private property or a
+ * public property
+ *
+ * setter is used to set the property but we can't return anything from the
+ * setter in the ts world and does not write return type in ts in setters
+ *
+ */
