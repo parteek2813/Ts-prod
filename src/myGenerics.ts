d053270164143e2bfecc64d8b3cc0caae7fa12e3
parteek kumar <digitalparteek@gmail.com>
2023-08-05 23:14:03 +0530

    generics class in ts

--- src/myGenerics.ts
+++ src/myGenerics.ts
@@ -26,3 +26,49 @@ interface Bottle {
 }
 
 // identityFour<Bottle>({});
+
+function getSearchProducts<T>(products: T[]): T {
+  // do some db operations
+  const myIndex = 3;
+  return products[3];
+}
+
+const getMoreSearchProducts = <T>(products: number[]): number => {
+  // do some db operations
+  const myIndex = 4;
+  return products[myIndex];
+};
+
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+// anotherFunction(3, {});
+
+interface Quiz {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
+}
+
+class Sellable<T> {
+  public cart: T[] = [];
+
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
+}
